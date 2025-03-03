1ff875137472c8aa1bf3a9ce75917ef723672da1
Dan Efran <dev@efran.org>
2025-03-03 01:17:00 -0500

    Notes: status: DAC isn't working. need a new one? Hardware lacks output buffers, but not much else!

--- first modules/MIDI Input/skiff_MIDI_in/skiff_MIDI_in.ino
+++ first modules/MIDI Input/skiff_MIDI_in/skiff_MIDI_in.ino
@@ -22,7 +22,14 @@
       apparently, so the '-' output pad is almost unsolderable.
       The Stemma QT port seems to work with other i2c boards, so I'll
       have to try another DAC board. [2025-03-03]
-
+    - Hardware is otherwise in good shape, but each output needs some
+      kind of buffering: the audio is probably close to an acceptable
+      level (?) but should probably be buffered; the Gate output 
+      should probably be raised to 5 or 10 v; the DAC output (when I
+      have one that works) will probably need to be doubled or even
+      tripled to give a decent octave range; that will count as a
+      buffer as well. Then the speaker will need a small amplifier,
+      with a volume attenuator knob. 
 */
 
 #include "display.h"
